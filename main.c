3f1d4b2efabd32fdd02f660297b5f2ce717c210f
Shane M. Pelletier <lotios611@gmail.com>
2013-12-25 14:57:09 -0400

    Add file reading

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int main() {
-	printf("Welcome to ChocoMinty.\n");
 	FILE *file;
+	long length;
+	char *buffer;
+	printf("Welcome to ChocoMinty.\n");
 	file = fopen("example.minty", "r");
-	if (!file) {
+	if (file == NULL) {
 		return 1;
 	}
+	fseek(file, 0, SEEK_END);
+	length = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	buffer = (char *)malloc(length);
+	fread(buffer, length, 1, file);
 	fclose(file);
+	printf("%s", buffer);
 	return 0;
 }
